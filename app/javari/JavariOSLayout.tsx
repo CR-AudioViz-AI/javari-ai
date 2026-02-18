de7ede4ad4f02823ed2c27a2e9a5a06402b53175
Old <225056366+roy-henderson@users.noreply.github.com>
2026-02-18 16:40:37 -0500

    Phase 2: JavariOSLayout - pass avatarState + pendingSpeech

--- app/javari/JavariOSLayout.tsx
+++ app/javari/JavariOSLayout.tsx
@@ -3,8 +3,14 @@
 import LeftSidebar from "./components/Sidebar/LeftSidebar";
 import RightSidebar from "./components/Sidebar/RightSidebar";
 import AvatarContainer from "./components/Avatar/AvatarContainer";
+import { useJavariState } from "./state/useJavariState";
+import { determineAvatarState } from "./components/Avatar/AvatarStateMachine";
 
 export default function JavariOSLayout({ children }: { children: React.ReactNode }) {
+  const { streaming, pendingSpeech, clearPendingSpeech } = useJavariState();
+
+  const avatarState = determineAvatarState({ streaming });
+
   return (
     <div className="w-screen h-screen overflow-hidden bg-black text-white flex">
       {/* LEFT CONTROL CENTER */}
@@ -12,7 +18,11 @@ export default function JavariOSLayout({ children }: { children: React.ReactNode
 
       {/* CENTER APPLICATION VIEW */}
       <div className="flex-1 flex flex-col">
-        <AvatarContainer state="idle" />
+        <AvatarContainer
+          state={avatarState}
+          pendingSpeech={pendingSpeech}
+          onSpeechComplete={clearPendingSpeech}
+        />
         {children}
       </div>
 
